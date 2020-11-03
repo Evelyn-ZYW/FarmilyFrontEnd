14b4ad2c50bacac084430ae364b4125558217ec9
= <=>
2020-11-02 20:25:18 -0800

    start on pages

---
+++ react_native_app/MyApp/pages/ForumBoard/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+import ForumPost from '../comps/ForumPost';
+
+export default function Home() {
+    return <View>
+        <ForumPost />
+    </View>
+}
