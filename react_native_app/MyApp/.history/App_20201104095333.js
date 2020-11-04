4832dd96107825ce7a5ee906b4c1d43c48e8d8f7
Brittany McDonald <bmcdonald80@my.bcit.ca>
2020-11-04 09:59:33 -0800

    Fixed app.js file to resolve conflicts

--- react_native_app/MyApp/.history/App_20201104095333.js
+++ react_native_app/MyApp/.history/App_20201104095333.js
@@ -8,11 +8,14 @@
 
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-
+import Button from './comps/Button';
+// import Avatar from './comps/Avatar';
+// import InputDate from './comps/InputDate';
+// import InputTime from './comps/InputTime';
 
 const App = () => {
   return <View style={styles.container}>
-
+    <Button />
   </View>
 
 }
@@ -20,11 +23,10 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
      flex:1,
-    //  backgroundColor:"red",
-    //  justifyContent:"center",
-    //  alignItems:"center"
+     backgroundColor:"red",
+     justifyContent:"center",
+     alignItems:"center"
   }
 })
-
 export default App;
 // export {default} from './storybook'; 
