bf455d74777a9598a58c866f09fe8d77d5b2b576
homerli <homer.lee.0508@gmail.com>
2020-11-08 20:23:44 -0800

    chat interface

--- react_native_app/MyApp/comps/Chat/index.js
+++ react_native_app/MyApp/comps/Chat/index.js
@@ -1,39 +1,41 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
 const ChatBubble = ({msg, background}) => {
   return (
     <View style={styles.container} backgroundColor={background}>
-      <Text style={styles.text}>
-        {msg}
-      </Text>
+      {background == '#DEDEDE' ? (
+        <Text style={(styles.text, {color: '#000'})}>{msg}</Text>
+      ) : (
+        <Text style={styles.text}>{msg}</Text>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    position: "relative",
-    backgroundColor: "#C97064",
-    display: "flex",
-    justifyContent: "flex-end",
-    width: 300,
+    position: 'relative',
+    backgroundColor: '#C97064',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    maxWidth: 200,
     borderRadius: 20,
     padding: 18,
-    margin: 10
+    margin: 10,
   },
 
   text: {
-    color: "#FFFFFF",
-    fontWeight: "300",
+    color: '#FFFFFF',
+    fontWeight: '300',
     fontSize: 17,
-    fontFamily: "Roboto"
-  }
+  },
 });
 
 ChatBubble.defaultProps = {
-  msg: "Hi! My name is Tig! I saw your post, that you're selling your John Deere Tractor. I'm interested in buying it from you!",
-  background: "#C97064"
-}
+  msg:
+    "Hi! My name is Tig! I saw your post, that you're selling your John Deere Tractor. I'm interested in buying it from you!",
+  background: '#C97064',
+};
 
 export default ChatBubble;
